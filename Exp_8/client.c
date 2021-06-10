d3843fb46a39efa6e6a2e31c29f35d25d794d720
Gautham P R <gauthampr18@gmail.com>
2021-06-10 11:55:40 +0530

    Initial

--- Exp_8/client.c
+++ Exp_8/client.c
@@ -7,27 +7,35 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define PORT	 8080
 #define MAXLINE 1024
 
-int main(int argc, char **argv){
+int main() {
+	int sockfd;
+	char buffer[MAXLINE];
+	char msg[100];
+	struct sockaddr_in servaddr;
 
-  int sockfd, connfd;
-  char msg[100];
-  char buffer[MAXLINE];
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  struct sockaddr_in myaddr;
-  myaddr.sin_family = AF_INET;
-  myaddr.sin_port = htons(5100);
-  myaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
 
-  bzero(msg, sizeof(msg));
-  printf("Enter the message: ");
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(PORT);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+
+  int len = sizeof(servaddr);
+  int n;
+  printf("Enter message: ");
   scanf("%[^\n]%*c", msg);
-  sendto(sockfd, msg, sizeof(msg), MSG_CONFIRM, (struct sockaddr *)&myaddr, sizeof(myaddr));
-  printf("Message send\n");
-  bzero(msg, sizeof(msg));
-  recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&myaddr, sizeof(myaddr));
-  printf("FROM SERVER: %s", msg);
-  printf("END\n");
-  close(sockfd);
+	sendto(sockfd, (const char *)msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *) &servaddr, len);
+	printf("Message sent.\n");
+
+  n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+  buffer[n] = '\0';
+	printf("Server : %s\n", buffer);
+
+	close(sockfd);
+	return 0;
 }
--- Exp_8/server.c
+++ Exp_8/server.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-# define MAXLINE 1024
+#define PORT	 8080
+#define MAXLINE 1024
 
-int main(int argc, char **argv){
+int main() {
+	int sockfd;
+	char buffer[MAXLINE];
+	char msg[100];
+	struct sockaddr_in servaddr;
 
-  int sockfd;
-  char msg[100];
-  char buffer[MAXLINE];
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  struct sockaddr_in myaddr;
-  myaddr.sin_family = AF_INET;
-  myaddr.sin_port = htons(5100);
-  myaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
 
-  int len = sizeof(myaddr);
-  if(bind(sockfd, (struct sockaddr*)&myaddr, sizeof(myaddr))!=-1){
-    bzero(msg, sizeof(msg));
-    recvfrom(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&myaddr, sizeof(myaddr));
-    printf("MESSAGE FROM CLIENT: %s\n", msg);
-    bzero(msg, sizeof(msg));
-    strcpy(buffer, "RECEIVED");
-    sendto(sockfd, (char *)buffer, MAXLINE, MSG_CONFIRM, (struct sockaddr *)&myaddr, sizeof(myaddr));
-    printf("Program ended\n");
-  }else{
-    printf("Error binding socket\n");
-  }
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+  int len = sizeof(servaddr);
+  int n;
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &servaddr, &len);
+  buffer[n] = '\0';
+	printf("Client : %s\n", buffer);
+  printf("Enter message: ");
+  scanf("%[^\n]%*c", msg);
+	sendto(sockfd, (const char *)msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *) &servaddr, len);
+	printf("Message sent.\n");
+
+	return 0;
 }
